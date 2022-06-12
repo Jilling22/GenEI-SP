ba0b2790fc3b0dafb77b4bf3ad8ac695ee820f11
Weepy <47512262+qLigature@users.noreply.github.com>
2022-06-12 22:34:18 +0800

    refactored character data to new file

--- main.ts
+++ main.ts
@@ -4,54 +4,6 @@ namespace SpriteKind {
     export const Cursor = SpriteKind.create()
 }
 
-type CharacterData = {
-    name: string,
-
-    normalSprite: Image,
-    sbaldSprite: Image,
-    baldSprite: Image,
-
-    normalWalkAnim: Image[],
-    sbaldWalkAnim: Image[],
-    baldWalkAnim: Image[],
-
-    normalHurtAnim: Image[],
-    sbaldHurtAnim: Image[],
-    baldHurtAnim: Image[]
-}
-
-const MIKAGE: CharacterData = {
-    name: "MIKAGE",
-
-    normalSprite: assets.image`Mikage`,
-    sbaldSprite: assets.image`MikageSemi`,
-    baldSprite: assets.image`MikageBald`,
-
-    normalWalkAnim: assets.animation`Mikage Walk`,
-    sbaldWalkAnim: assets.animation`MikageSemi Walk`,
-    baldWalkAnim: assets.animation`MikageBald Walk`,
-
-    normalHurtAnim: assets.animation`Mikage Injured`,
-    sbaldHurtAnim: assets.animation`MikageSemi Injured`,
-    baldHurtAnim: assets.animation`MikageBald Injured`
-}
-
-const SPICA: CharacterData = {
-    name: "SPICA",
-
-    normalSprite: assets.image`Spica`,
-    sbaldSprite: assets.image`SpicaSemi`,
-    baldSprite: assets.image`SpicaBald`,
-
-    normalWalkAnim: assets.animation`Spica Walk`,
-    sbaldWalkAnim: assets.animation`SpicaSemi Walk`,
-    baldWalkAnim: assets.animation`SpicaBald Walk`,
-
-    normalHurtAnim: assets.animation`Spica Injured`,
-    sbaldHurtAnim: assets.animation`SpicaSemi Injured`,
-    baldHurtAnim: assets.animation`SpicaBald Injured`
-}
-
 class Player {
     spriteAssets: CharacterData
     sprite: Sprite
