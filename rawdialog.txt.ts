3dffaab1522fa4ab3212d63345205b156f9934fb
Jilling22 <107066832+Jilling22@users.noreply.github.com>
2022-06-29 12:24:34 -0700

    add dialogue in stage 2

--- rawdialog.txt.ts
+++ rawdialog.txt.ts
@@ -50,18 +50,188 @@ for it, but why?
 
 
 // STAGE 2 INTRO
+`Mikage
+---------------
+I have to hurry now…
+Mikage
+---------------
+I can't let Spica and
+Mikage
+---------------
+the others see me like this.
+    `;
 
+`Spica
+---------------
+This place is giving me the creeps,
+Spica
+---------------
+but I gotta do my best for her!
+    `;
+    
+`Yuuhi
+---------------
+What did that ice cream girl
+Yuuhi
+---------------
+say again?
+Yuuhi
+---------------
+Was it left or right…
+Yuuhi
+---------------
+Wait, where am I?
+ `;
 
+`Urara
+---------------
+Zzz…
+Urara
+---------------
+(If only Spica or Yuuhi were here,
+Urara
+---------------
+I'd still be sleeping
+Urara
+---------------
+in bed right now…)
+ `;
 
+`
+
+ `;
 
 // PHANTOM INTROS
+`Phantom
+---------------
+Did you come here to give up?
+Mikage
+---------------
+I'm not here to play games with you.
+Mikage
+---------------
+Where did you hide the album?
+Phantom
+---------------
+Mikage, you never needed that album
+Phantom
+---------------
+to begin with.
+Phantom
+---------------
+It's okay to be bald.
+Phantom
+---------------
+Spica and the others will accept you
+Phantom
+---------------
+regardless
+Phantom
+---------------
+Isn't that what true friends are for?
+Mikage
+---------------
+This is completely different!
+Mikage
+---------------
+I'll never be bald!
+Mikage
+---------------
+Never ever ever!
+    `;
 
+`
+Phantom
+---------------
+... begone, Spica.
+Phantom
+---------------
+The forest of shadows does not take
+Phantom
+---------------
+kindly to the blinding light.
+Spica
+---------------
+Light? I don't see any here…
+Spica
+---------------
+By the way,
+Spica
+---------------
+did you happen to know where the
+Spica
+---------------
+album that can cure baldness is?
+Phantom
+---------------
+That was your last warning.
+Phantom
+---------------
+Now you too shall experience baldness.
+    `;
 
+`Phantom
+---------------
+I know what you did.
+Phantom
+---------------
+Don't think you can get away from us.
+Yuuhi
+---------------
+Do what now?
+Phantom
+---------------
+Don't play dumb with me,
+Phantom
+---------------
+you murderer.
+Phantom
+---------------
+It's about time you paid the price,
+Phantom
+---------------
+with your hair.
+    `;
+    
+`Phantom
+---------------
+Lord Urara, have mercy on us.
+Urara
+---------------
+Zzz…
+Urara
+---------------
+(Cease your useless begging.
+Urara
+---------------
+I will exterminate you and your kind,
+Urara
+---------------
+on behalf of my friend.)
+Phantom
+---------------
+…
+ `;
 
+`
+
+ `;
 
 // PHANTOM DEATH
+`Phantom
+---------------
+PH'NGLUIMGLW'NAFHCTHULHUR'LYEHWGAH'NA
+Phantom
+---------------
+… the album…
+Phantom
+---------------
+is not with me anymore…
+ `;
 
+`
 
+ `;
 
 // STAGE 3 INTRO
 
